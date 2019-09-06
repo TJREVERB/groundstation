b76dd58b62619046dd7f733aa8db18b1e12cde37
Edward Li <Legendeddie18@gmail.com>
2019-09-06 18:22:20 -0400

    Remove duplicated files

--- gsMaster.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-listen = subprocess.Popen(['gnome-terminal', '-x', 'python3', 'listen.py'])
-transmit = subprocess.Popen(['gnome-terminal', '-x', 'python3', 'transmit.py'])
-fullGroundstation = subprocess.Popen(['gnome-terminal', '-x', 'python3', 'TJ_Groundstation_USRP.py'])
-listen.wait()
-transmit.wait()
-fullGroundstation.wait()
