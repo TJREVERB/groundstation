a03034f31f6fa101f3c771e80206e63d177417c9
Edward Li <Legendeddie18@gmail.com>
2019-09-26 18:30:08 -0400

    Updated files

---
+++ test.py
@@ -0,0 +1,23 @@
+from flask import Flask, url_for, request
+app = Flask(__name__)
+
+@app.route('/')
+def api_root():
+    return 'Welcome'
+@app.route("/hello")
+def api_hello():
+    if 'name' in request.args:
+        return 'Hello ' + request.args['name']
+    else:
+        return 'Hello John Doe'
+
+@app.route('/articles')
+def api_articles():
+    return 'List of ' + url_for('api_articles')
+
+@app.route('/articles/<articleid>')
+def api_article(articleid):
+    return 'You are reading ' + articleid
+
+if __name__ == '__main__':
+    app.run()
