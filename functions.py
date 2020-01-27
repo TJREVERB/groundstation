1b5a2454a2e63ada81abf54374457f87f1bccaa3
Edward Li <Legendeddie18@gmail.com>
2020-01-27 11:23:49 -0500

    Cleaned up code

--- functions.py
+++ functions.py
@@ -1,26 +1,55 @@
 from threading import Thread
 import logging
 import time
-# import sys
 import socket
 import datetime
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-#logging.basicConfig(filename='event_log.log',level=logging.DEBUG)
-messageList = []
-'''
-    SEND MESSAGE:
-    in_module
-    in_method
-    check_args
-    send
-    '''
-# import subprocess
+
+class listen_class:
+    def __init__(self, listenList):
+        self.messageList = listenList
+        
+    def get_list(self):
+        return self.messageList
+
+    def reset_list(self):
+        self.messageList = []
+
+    def start_listen(self):
+        t1 = Thread(target=self.listen, args=())
+        t1.daemon = True
+        t1.start()
+
+    def listen_test(self):  # FOR TESTING
+        while True:
+            message = input("What is your message?")
+            self.messageList.append(message)
+            print(self.messageList)
+
+    def listen(self):
+        UDP_IP = "127.0.0.1"
+        RX_PORT = 5557
+        while True:
+            msg_lstn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+            msg_lstn.bind((UDP_IP, RX_PORT))
+            ack, addr = msg_lstn.recvfrom(1024)
+            if "to SATT4" in str(ack): #checks if message received was the one we just sent
+                print("RX: ", end="")
+                print(ack)
+                self.messageList.append(ack)
+                timestamp = get_time()
+                city_ref = db.collection(u'Log').document(u'Listen')
+                city_ref.update({
+                    timestamp : ack,
+                })
+            checksum = generate_checksum(ack)
+            time.sleep(1)
 cred = credentials.Certificate("groundstation-listen-log-firebase-adminsdk-h2jfr-9d71ce0bdb.json")
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
+
 tx_port = 5555
 udp_ip = "127.0.0.1"
 
@@ -69,23 +98,16 @@ submodules = {
     '_init_': _init_
 }
 
-
 def get_time():  # get_time()
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
     return (st)
-
-
-#def get_logger():
-    #return (logging.getLogger("CI"))
-
 
 def print_arg(list):
     string = ""
     for arg in list:
         string += str(arg) + ","
     return (string[:-1])
-
 
 def generate_checksum(body: str):
     """
@@ -97,8 +119,6 @@ def generate_checksum(body: str):
     sum1 = sum([ord(x) for x in body[0:-7]])
     sum1 %= 26
     sum1 += 65
-    #logger = get_logger()
-    #logger.debug('CHECKOUT :' + chr(sum1) + ";")
     return chr(sum1)
 
 def check_checksum (body: str):
@@ -106,101 +126,26 @@ def check_checksum (body: str):
     checksum = generate_checksum(body2)
     if (checksum == body[-1]):
         return True;
-    else: return False;
-class listen_class:
-    def __init__(self, listenList):
-        self.messageList = listenList
-        # self.messageList.append(messageList)
+    else: 
+        return False;
 
-    def get_list(self):
-        return self.messageList
-
-    def reset_list(self):
-        self.messageList = []
-
-    def start_listen(self):
-        t1 = Thread(target=self.listen_test, args=())
-        t1.daemon = True
-        t1.start()
-
-    def listen_test(self):  # FOR TESTING
-        while True:
-            message = input("What is your message?")
-            self.messageList.append(message)
-            print(self.messageList)
-
-    def listen(self):
-        UDP_IP = "127.0.0.1"
-        RX_PORT = 5557
-        while True:
-            msg_lstn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-            msg_lstn.bind((UDP_IP, RX_PORT))
-            ack, addr = msg_lstn.recvfrom(1024)
-            #print (ack)
-            #time.sleep(1)
-            #REDUDANCY CHECK
-            if "to SATT4" in str(ack):
-                # getTime()
-                print("RX: ", end="")
-                print(ack)
-                self.messageList.append(ack)
-                timestamp = get_time()
-                city_ref = db.collection(u'Log').document(u'Listen')
-                city_ref.update({
-                    timestamp : ack,
-                })
-            checksum = generate_checksum(ack)
-            time.sleep(1)
-            #self.messageList.append(ack)
-                # return(str(ack))
-            #print ("RX: " + get_time(), end="")
-            #print (ack)
-            # print(ack)
-            # self.messageList.append(ack)
 def in_module(module):
-    # get_time()
-    # submod = input("UI: Which Module?\n")
-    # while(submod not in submodules):
-    # get_time()
-    # submod = input("UI: Which Module?\n")
-    # return(submod)
     if (module in submodules):
         return True
     return False
 
-
-def print_Methods(submodule):
-    methods = "<font color=green></br>"
-    for i in submodules[submodule].keys():
-        print(i)
-        methods += i + "</br>"
-    methods += "</font>"
-    return (methods)
-
-
 def in_method(submod, method):
-    '''method = input("UI: Which method?\n")
-        while(method not in submodules[submod]):
-        get_time()
-        method = input("UI: Which method?\n")
-        return(method)'''
     if (method in submodules[submod]):
         return True
     return False
 
-
 def check_int(num):
     return (num.isdigit())
 
-
 def check_bool(booleanVar):
-    # takes in str
     if (booleanVar == "True" or booleanVar == "False"):
         return (True)
     return (False)
-
-
-# def send_message()
 
 def check_float(floatVar):
     try:
@@ -209,24 +154,10 @@ def check_float(floatVar):
     except ValueError:
         return (False)
 
-
 def check_char(charVal):
     if (charVal.isalpha() == True and len(charVal) <= 1):
         return (True)
     return False
-
-
-def arg_length(module, method):
-    return (len(submodules[module][method]))
-
-
-def get_arg(module, method):
-    argStr = "<font color=green></br>Args: ["
-    for i in (submodules[module][method]):
-        argStr += i + ", "
-    argStr += "]<font>"
-    return (argStr)
-
 
 def check_args(module, method, argList):
     argumentlist = submodules[module][method]
@@ -234,53 +165,37 @@ def check_args(module, method, argList):
         return (False)
     count = 0
     for i in argumentlist:
-        # print(count)
-        # print(argList[count])
         if (i == "int"):
             if (check_int(argList[count]) == False):
-                # print("int")
                 return (False)
             count += 1
         elif (i == "bool"):
             if (check_bool(argList[count]) == False):
-                # print("bool")
                 return (False)
             count += 1
         elif (i == "float"):
             if (check_float(argList[count]) == False):
-                # print("float")
                 return (False)
             count += 1
         elif (i == "char"):
             if (check_char(argList[count]) == False):
-                # print("char")
                 return (False)
             count += 1
         else:
             count += 1
     return (True)
 
-
-def get_message(module, method, argList):
-    checksum = generate_checksum(
-        'TJ' + module + ',' + method + ',' + print_arg(argList))
-    msg = "TJ" + module + "," + method + "," + print_arg(argList) + checksum
-    return (msg)
-
-
 def send(module, method, argList):  # ASSUMES EVERYTHING HAS BEEN CHECKED
-    log = open("event_log.log", "a")
     checksum = generate_checksum('TJ' + module + ',' + method + ',' + print_arg(argList))
     msg = "TJ" + module + "," + method + "," + print_arg(argList) + checksum
     try:  # Message successfully sent
         msg_snd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         msg_snd.sendto(msg.encode(), (udp_ip, tx_port))
-        city_ref = db.collection(u'Log').document(u'Send')
+        city_ref = db.collection(u'Log').document(u'Send')#
         timestamp = get_time()
-        city_ref.update({
-            timestamp : msg,
-        })
+        city_ref.update({#
+            timestamp : msg,#
+        })#
         return True
     except:
-        log.write("ERROR: MESSAGE FAILED TO SEND. MESSAGE: " + msg)
         return False
--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import logging
 import functions
 
 app = Flask(__name__)
-log = logging.getLogger('werkzeug')
-log.disabled = True
 message = []
 failed_msg = [] #list of every message that has an incorrect checksum
 @app.route("/send", methods=["POST"])
@@ -21,19 +19,18 @@ def handle_send():
     else:
         return "incorrect parameters", 405
 
-
 @app.route("/")
-def send():
+def template():
     return render_template("index.html")
 
 
 @app.route("/listen", methods=['GET'])
 def listen():
-    listen_list = myobject.get_list()
-    for i in listen_list:
-        if (listen_list[i].check_checksum is not True):
-            failed_msg.append(i)
-    myobject.reset_list()
+    listen_list = listenObject.get_list()
+    #for i in listen_list:
+        #if (listen_list[i].check_checksum is not True):
+            #failed_msg.append(i)
+    listenObject.reset_list()
     return jsonify(listen_list)
 
 @app.route("/failed", methods=['GET'])
@@ -41,15 +38,8 @@ def checksum():
     return_list = failed_msg
     failed_msg.clear()
     return return_list
-#returns all elements of failed_msg
-#clears failed_msg
+
 if __name__ == '__main__':
-    '''t1 = Thread(target = listen_thread, args = ())
-    t1.daemon = True
-    t1.start()'''
-    myobject = functions.listen_class([])
-    myobject.start_listen()
-    '''functions.start_listen()
-    print(functions.get_time(), "yes")
-    print("done")'''
+    listenObject = functions.listen_class([])
+    listenObject.start_listen()
     app.run()
