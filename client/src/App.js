18f70e502f69ec4d173e416229f5359028981ad9
Akash Bhave <akashbbhave@gmail.com>
2020-02-27 18:32:34 -0500

    Install Firebase on /client

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase';
+
+const config = {};
+firebase.initializeApp(config);
+
+const uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+    signInSuccessUrl: '/signedIn',
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID, firebase.auth.FacebookAuthProvider.PROVIDER_ID]
+};
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <p>Test</p>
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
         </div>
     );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as firebaseui from 'firebaseui';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
