b9cafc251a6c26c325aaa0d7e03260705817bc4b
Akash Bhave <akashbbhave@gmail.com>
2020-03-05 16:23:38 -0500

    Uninstall Firebase UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,9 @@
 import React from 'react';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import firebase from 'firebase';
-
-
-const config = {
-    apiKey: process.env.API_KEY,
-    authDomain: 'localhost:3000'
-};
-firebase.initializeApp(config);
-
-const uiConfig = {
-    // Popup signin flow rather than redirect flow.
-    signInFlow: 'popup',
-    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-    signInSuccessUrl: '/signedIn',
-    // We will display Google and Facebook as auth providers.
-    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID, firebase.auth.FacebookAuthProvider.PROVIDER_ID]
-};
 
 function App() {
     return (
         <div className="App">
             <p>Test</p>
-            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
         </div>
     );
 }
