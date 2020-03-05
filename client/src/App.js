bdbf5e778b43e58723ebd32ab6e451c798046248
Akash Bhave <akashbbhave@gmail.com>
2020-03-05 17:52:16 -0500

    Configure Firebase to use Cloud Firestore

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import 'firebase/firestore';
 
-function App() {
+const App = ({ firebase }) => {
+    const db = firebase.firestore();
+    const test = () => {
+        db.collection('inbound')
+            .get()
+            .then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+                });
+            });
+    };
+
     return (
         <div className="App">
             <p>Test</p>
+            <button onClick={test}>Test</button>
         </div>
     );
-}
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,26 @@
-require('dotenv').config();
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as firebaseui from 'firebaseui';
+import * as firebase from 'firebase/app';
 
+import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './App';
 
-import * as serviceWorker from './serviceWorker';
+require('dotenv').config();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: 'groundstation-ecf9f.firebaseapp.com',
+    databaseURL: 'https://groundstation-ecf9f.firebaseio.com',
+    projectId: 'groundstation-ecf9f',
+    storageBucket: 'groundstation-ecf9f.appspot.com',
+    messagingSenderId: process.env.FIREBASE_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render(<App firebase={firebase} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
